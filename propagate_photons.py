c080bd696c7d341e370c9c36809d40df65f880d9
Christian Haack <christian.haack@tum.de>
2021-11-03 16:37:10 +0100

    notebook updates and some documentation for example propagation script

--- propagate_photons.py
+++ propagate_photons.py
@@ -1,26 +1,34 @@
 import jax.numpy as jnp
-from jax import jit, vmap
+from jax import jit, vmap, random
 from hyperion.propagate import (
     photon_sphere_intersection,
     mixed_hg_rayleigh,
     make_step_function,
-    collect_hits,
 )
+import json
 
-sca_len = 100
-c_medium = 0.3 / 1.35
-abs_len = 30
-r = 0.21
-det_pos = jnp.array([0, 0, 100])
+# Load medium properties
+medium = json.load(open("resources/medium.json"))
+c_medium = 0.299792458 / medium["n_ph"]
+
+# Define emitter position & emission time
 emitter_x = jnp.array([0, 0, 0.0])
 emitter_t = 0.0
 
+# Define detector position & sensor radius (sensor is modelles as a sphere)
+det_pos = jnp.array([0, 0, 100.0])
+det_radius = 0.21
+
+# The emission direction is not used when photons are emitted isotropically
+emitter_dir = jnp.array([0, 0, 1.0])
+
+# Make a step function
 fun = make_step_function(
-    1 / sca_len,
+    1 / medium["sca_len"],
     c_medium,
     det_pos,
-    r,
-    abs_len * 10,
+    det_radius,
+    medium["abs_len"] * 15,  # maximum propagation distance for a photon
     emitter_x,
     emitter_t,
     mode="uniform",
@@ -28,4 +36,20 @@ fun = make_step_function(
     scattering_function=mixed_hg_rayleigh,
 )
 make_n_steps = jit(vmap(fun, in_axes=[0]))
-isec_times, ph_thetas, stepss, isec_poss = collect_hits(make_n_steps, 1e7, 100)
+
+key = random.PRNGKey(0)
+
+isec_times = []
+ph_thetas = []
+stepss = []
+nphotons = int(1e7)
+isec_poss = []
+
+(
+    positions,
+    times,
+    directions,
+    has_intersected,
+    intersection_position,
+    steps,
+) = make_n_steps(random.split(key, num=nphotons))
