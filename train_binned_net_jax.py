11496c70a5b80754e5de6e7657c024c1ec7c9138
Christian Haack <christian.haack@tum.de>
2022-01-04 14:25:36 +0100

    update notebooks

--- train_binned_net_jax.py
+++ train_binned_net_jax.py
@@ -10,7 +10,11 @@ import jax.numpy as jnp
 import numpy as np
 import optax
 from torch.utils.tensorboard import SummaryWriter
-from hyperion.models.photon_binned_amplitude.net import make_forward_fn, train_net
+from hyperion.models.photon_binned_amplitude.net import (
+    make_forward_fn,
+    train_net,
+    make_logp1_trafo,
+)
 from hyperion.data import SimpleDataset, create_random_split
 
 tts = 2
@@ -18,17 +22,16 @@ hists = pickle.load(open(f"data/combined_photon_hists_{tts}.pickle", "rb"))
 inputs = np.asarray(hists[::2])
 outputs = np.asarray(hists[1::2])
 inputs = inputs.reshape((inputs.shape[0] * inputs.shape[1], 2))
-outputs = outputs.reshape((outputs.shape[0] * outputs.shape[1], 529))
+outputs = outputs.reshape((outputs.shape[0] * outputs.shape[1], outputs.shape[-1]))
 
-outputs = np.log(outputs)
+trafo_scale = 1e18
+trafo, rev_trafo = make_logp1_trafo(trafo_scale)
 
-first_finite = np.nonzero(
-    np.sum(~np.isfinite(outputs), axis=0) / outputs.shape[0] < 0.5
-)[0][0]
-# print(first_finite)
-outputs[:, :first_finite] = -300
+outputs = trafo(outputs)
 
-binning = jnp.arange(-30, 500, 1)
+binning = jnp.concatenate(
+    [jnp.linspace(0, 10, 11), jnp.logspace(1, jnp.log10(500), 25)[1:]]
+)
 
 
 inputs[:, 1] = jnp.log10(inputs[:, 1])
@@ -38,9 +41,9 @@ rng = np.random.RandomState(2)
 train_data, test_data = create_random_split(data, split, rng)
 
 
-for n_neurons in [1000]:
-    for epochs in [700, 900, 1000]:
-        for lr in [0.007, 0.01, 0.02]:
+for n_neurons in [300, 500, 1500]:
+    for epochs in [200, 400, 700]:
+        for lr in [0.001, 0.005, 0.01]:
             for dropout in [0, 0.2, 0.3]:
                 conf = {
                     "batch_size": 500,
@@ -50,12 +53,18 @@ for n_neurons in [1000]:
                     "lr": lr,
                     "epochs": epochs,
                     "n_neurons": n_neurons,
+                    "final_activations": jax.nn.softplus,
                 }
-            ident_str = f"hist_{conf['batch_size']}_{conf['lr']}_{conf['epochs']}_{conf['dropout']}_{conf['n_neurons']}"
-            writer = SummaryWriter(os.path.join("/tmp/tensorboard/runs/", ident_str))
+                ident_str = f"hist_{conf['batch_size']}_{conf['lr']}_{conf['epochs']}_{conf['dropout']}_{conf['n_neurons']}"
+                writer = SummaryWriter(
+                    os.path.join("/tmp/tensorboard/runs/", ident_str)
+                )
 
-            net, params, state = train_net(conf, train_data, test_data, writer, rng)
-            pickle.dump([params, state], open(f"data/{ident_str}.pickle", "wb"))
+                net, params, state = train_net(conf, train_data, test_data, writer, rng)
+                pickle.dump(
+                    [params, state, conf, binning, trafo_scale],
+                    open(f"data/{ident_str}.pickle", "wb"),
+                )
 
 
 """
@@ -75,7 +84,7 @@ writer = SummaryWriter(
 
 
 net, params, state = train_net(conf, train_data, test_data, writer, rng)
-"""
+
 pickle.dump([params, state, conf], open("data/arrival_hist_net_2tts_jax.pickle", "wb"))
 
 params, state, conf = pickle.load(open("data/arrival_hist_net_2tts_jax.pickle", "rb"))
@@ -86,3 +95,4 @@ net = hk.transform_with_state(forward_fn)
 @jax.jit
 def net_eval_fn(x):
     return net.apply(params, state, None, x, is_training=False)[0]
+"""
