69e7e048540baac885a4dbdea8b6f000eef0ca10
Christian Haack <chr.hck@gmail.com>
2022-01-04 09:26:49 +0000

    bugfixes

--- combine_normflow_datasets.py
+++ combine_normflow_datasets.py
@@ -5,10 +5,17 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("-i", "--infiles", dest="infiles", nargs="+", required=True)
 parser.add_argument("-o", "--outfile", dest="outfile", required=True)
+parser.add_argument("-s", "--seed", dest="seed", type=int, required=True)
 args = parser.parse_args()
+
+rstate = np.random.RandomState(args.seed)
 
 data = []
 for f in args.infiles:
-    data.append(pickle.load(open(f, "rb")))
+    indata =  pickle.load(open(f, "rb"))
+    size = int(min(5E6, indata.shape[1]))
+    choice = rstate.choice(np.arange(indata.shape[1]), size=size, replace=False)
+    data.append(indata[:, choice])
 data = np.hstack(data)
-pickle.dump(data, open(args.outfile, "wb"))
+
+np.savez(args.outfile, data)
